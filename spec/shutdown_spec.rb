06cb3b9840d30413a884089b0c981801914810d8
Pavlo <pavlo@kerestey.net>
2012-03-29 19:01:26 +0200

    shutdown_spec.rb:18: warning: don't put space before argument parentheses

--- spec/shutdown_spec.rb
+++ spec/shutdown_spec.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require './spec/spec_tools.rb'
 include SpecTools
- 
+
 describe "shutdown" do
   before :each do
     @kozo_port = 10531
@@ -9,13 +9,13 @@ describe "shutdown" do
     Process.detach @kozo
     sleep 3
   end
-  
+
   it "Can be stopped via HTTP call to /quit" do
     r = interpret_result `curl -sS http://localhost:#{@kozo_port}/quit`
     r.should == (ok_msg "Quitting")
     sleep 1
     pids = `ps aux | grep [p]hantom| awk '{print $2}'`
-    pids.split("\n").should_not include (@kozo.to_s)
+    pids.split("\n").should_not include @kozo.to_s
 
   end
 end
